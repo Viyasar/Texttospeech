a12c6dd768a8d516c6898b32c2aaf9d1cc24a0ee
Viyasar <126664695+Viyasar@users.noreply.github.com>
2023-07-20 13:57:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let speech=new SpeechSynthesisUtterance();
+document.querySelector("button").addEventListener("click",()=>{
+    speech.text=document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);});
